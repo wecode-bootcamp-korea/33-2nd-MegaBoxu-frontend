0ebb37d381e57ba4e6f69b9b223eb39a44ff0119
Sorin0404 <haewong27@gmail.com>
2022-06-16 17:39:48 +0900

    리뷰 수정 완료
    removeItem 변경, useEffect 원래대로, 삼항연산자 재사용 완료했습니다.

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import SignUp from './pages/SignUp/SignUp';
-import Login from './pages/Login/Login';
 import Main from './pages/Main/Main';
 import MovieList from './pages/MovieList/MovieList';
 import MovieDetail from './pages/MovieDetail/MovieDetail';
@@ -18,7 +17,6 @@ function Router() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/movielist" element={<MovieList />} />
         <Route path="/moviedetail" element={<MovieDetail />} />
         <Route path="/booking" element={<Booking />} />
